71036a8d9f3e526251cb6e002eea03fecdecab8f
Youssef Haboul <haboulyoussef14@gmail.com>
2024-03-10 11:54:20 +0000

    m

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,7 @@ class BaseModel:
         """
         self.updated_at = datetime.today()
 
-    def to_dic(self):
+    def to_dict(self):
         """
             Returns a dictionary containing all keys/values
             of __dict__ of the instance.
